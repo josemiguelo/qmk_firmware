586f31f272daa8de4f7f53846033b3fe025f5223
Jose Miguel Ochoa <josemiguel.ochoa@kushkipagos.com>
2023-02-01 18:48:58 -0500

    replace ralt with lalt on main layer

--- keyboards/crkbd/rev1/keymaps/jm/keymap.c
+++ keyboards/crkbd/rev1/keymaps/jm/keymap.c
@@ -31,14 +31,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |----------+---+---+---+---+---|                  |---+---+----+----+----+----+------|
   * |LShift/Esc| Z | X | C | V | B |                  | N | M | ,< | .> | /? |RShift/"\" |
   * `----------+---+---+---+---+---+-----.  .---------+---+---+----+----+----+-----------'
-               | Numpad|  Wind | Nav/Ent |  |Sym/Space| Ralt  | Fn |
+               | Numpad|  Wind | Nav/Ent |  |Sym/Space| Lalt  | Fn |
                '--------+------+---------'  '---------+-------+----+                          
   */
   [0] = LAYOUT_split_3x6_3(
   LT(6,KC_TILD),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                 KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    KC_BSPC,
   LCTL_T(KC_TAB),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                 KC_H,    KC_J,   KC_K,    KC_L,  KC_SCLN,  RCTL_T(KC_QUOT),
   LSFT_T(KC_ESC),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                 KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  RSFT_T(KC_BSLS),
-                                        MO(3), KC_LGUI, LT(1,KC_ENT) , LT(2,KC_SPC), KC_RALT, MO(5)
+                                        MO(3), KC_LGUI, LT(1,KC_ENT) , LT(2,KC_SPC), KC_LALT, MO(5)
   ),
 
 
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |------+---+----+----+-----+----|                     |-----+----+----+-----+---+------|
   * |LShift|   |    | CUT|COPY|PASTE|                     |PREV |PLAY|NEXT|     |   |RShift|
   * `----------+---+---+---+---+---+---------.  .---------+---+---+----+----+---+---+------'
-                   | Numpad|  Wind |         |  |Sym/Space| Ralt  | Adj |
+                   | Numpad|  Wind |         |  |Sym/Space| Lalt  | Adj |
                    '--------+------+---------'  '---------+-------+-----'                          
   */
   [1] = LAYOUT_split_3x6_3(
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |-------+------+------+-----|                        |------------+------+-------|
   * |       |   |   |   |   | % |                        . 0 | 1 | 2 | 3 |   |       |
   * `----------+---+---+---+---+---+-----.  .---------+---+---+----+----+----+-------'
-               |       |  Wind | Nav/Ent |  |Sym/Space| Ralt  | Fn |
+               |       |  Wind | Nav/Ent |  |Sym/Space| Lalt  | Fn |
                '--------+------+---------'  '---------+-------+----+                          
   */
   [3] = LAYOUT_split_3x6_3(
@@ -140,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |------+-----+-----+----+----+----|                       |----+----+----+----+-----|------|
   * |LShift|     |     |    |    |    |                       |    |    |    |    |     |LShift|
   * `------+-----+-----+----+----+----+---------.   .---------+----+----+----+----+-----+------|
-                       | Numpad| Wind | Nav/Ent |   |Sym/Space|   Ralt  |    |
+                       | Numpad| Wind | Nav/Ent |   |Sym/Space|   Lalt  |    |
                        '--------+-----+---------'   '---------+---------+----'                          
   */
   [5] = LAYOUT_split_3x6_3(
